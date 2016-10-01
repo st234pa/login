6f9a829ebb4347eb482be7cd43ac7bb22136da90
STmb2 <STmb2@dyn-209-2-233-144.dyn.columbia.edu>
2016-10-01 12:00:56 -0400

    restructured auth, started hash, register, checkLogin

--- app.py
+++ app.py
@@ -2,20 +2,34 @@ from flask import Flask, render_template, request
 import hashlib
 
 app = Flask(__name__)
-example = {"user": "steph", "password": "yoon"}
+
 def hash(x):
-    return hashlib.sha256(
+    return hashlib.sha256(x.encode())
+
+def register(username, password):
+    return "You are now successfully registered."
+
+def checkLogin(username, password):
+    return "You are now successfully logged in."
+
 @app.route("/")
 @app.route("/login/") #multiple routes go to the same function/
 def login():
     print request.headers
     return render_template("login.html")
+        
 @app.route("/authenticate/", methods=['GET', 'POST'])
 def auth():
     response = request.form
-    if response["user"] == example["user"] and response["password"] == example["password"]:
-        return "You have successfully logged in"
-    return "Nope"
+    username = response['user']
+    password = response['password']
+    if response['submit'] == 'Register':
+        m1 = register(username, password)
+        return render_template("login.html", message = m1)
+    else:
+        m2 = checkLogin(username, password)
+        return render_templates("login.html", message = m2)
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
