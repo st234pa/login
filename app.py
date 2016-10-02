df14d83ecd512bc54551d862cbc894655a1ec1a5
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-10-02 14:26:11 -0400

    finished

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template, request
 import hashlib
+import csv
 
 app = Flask(__name__)
 
 def hash(x):
-    return hashlib.sha256(x.encode())
+    h = hashlib.sha1()
+    h.update(x)
+    return h.hexdigest()
 
 def register(username, password):
+    d = csv.reader(open("data/d.csv"))
+    for i in d:
+        if username == i[0]:
+            return "You are already registered."
+    with open('data/d.csv', 'a') as f:
+        w = csv.writer(f)
+        w.writerow([username, hash(password)])
     return "You are now successfully registered."
 
 def checkLogin(username, password):
-    return "You are now successfully logged in."
+    d = csv.reader(open("data/d.csv"))
+    for i in d:
+        if username == i[0]:
+            if i[1] == hash(password):
+                return "You are now successfully logged in."
+            return "Incorrect password."
+    return "Incorrect username."
 
 @app.route("/")
 @app.route("/login/") #multiple routes go to the same function/
@@ -23,7 +39,7 @@ def auth():
     response = request.form
     username = response['user']
     password = response['password']
-    if response['submit'] == 'Register':
+    if response['enter'] == 'Register':
         m1 = register(username, password)
         return render_template("login.html", message = m1)
     else:
