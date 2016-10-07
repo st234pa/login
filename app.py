1b548e310dc22fe0ca40aa9b1fe194d803606e91
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-10-07 14:33:16 -0400

    not finished but started to add session stuff

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import hashlib
 import csv
 
 app = Flask(__name__)
+#SESSION_TYPE = 'redis'
+#app.config.from_object(__name__)
+#Session(app)
+app.secret_key = '??_|??0??l6?J??8?J2????A???+q? '
 
 def hash(x):
     h = hashlib.sha1()
@@ -24,11 +28,15 @@ def checkLogin(username, password):
     for i in d:
         if username == i[0]:
             if i[1] == hash(password):
+                session[username] = hash(password)
                 return "You are now successfully logged in."
             return "Incorrect password."
     return "Incorrect username."
 
 @app.route("/")
+def root():
+    return redirect(url_for('login'))
+
 @app.route("/login/") #multiple routes go to the same function/
 def login():
     print request.headers
